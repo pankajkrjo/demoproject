6a74215f468b3a0960f24805ae4b353e50aca6e7
pankajkblite <139267014+pankajkblite@users.noreply.github.com>
2025-05-02 13:39:41 +0530

    1st commit

---
+++ src/String2.java
@@ -0,0 +1,48 @@
+import org.apache.commons.lang3.StringUtils;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+public class String2 {
+
+    public static void main(String[] args) {
+
+        String sr = "Pankaj_EM is good";
+        String[] str = StringUtils.split(sr, " ");
+        System.out.println(str[0]);
+        for (String directory : str) {
+            System.out.print(directory);
+
+  /// Users/pankajkumar/Documents/Practice
+            //int fileCount = 5; // Number of files to create
+            // String directory = "generated_files"; // Folder to store files
+
+            // Create directory if not exists
+            File dir = new File(directory);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            //  for (int i = 1; i <= fileCount; i++) {
+            for (int i = 1; i < str.length - 1; i++) {
+               String fileName = directory + "/file" + i + ".txt"; // Generate file name
+                //Users/pankajkumar/Documents/Practice
+                File file = new File(fileName);
+
+                try {
+                    if (file.createNewFile()) {
+                        System.out.println("File created: " + fileName);
+                        FileWriter writer = new FileWriter(file);
+                        writer.write("This is file " + i);
+                        writer.close();
+                    } else {
+                        System.out.println("File already exists: " + fileName);
+                    }
+                } catch (IOException e) {
+                    System.out.println("An error occurred while creating the file: " + fileName);
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
---
+++ src/com/java/collection/ArrayList/PersonisEmpty.java
@@ -0,0 +1,23 @@
+package com.java.collection.ArrayList;
+
+import java.util.ArrayList;
+
+public class PersonisEmpty {
+
+    String s;
+    int i;
+
+    public PersonisEmpty(String s, int i) {
+        this.s = s;
+        this.i = i;
+    }
+
+    public static void main(String args[]){
+        ArrayList<PersonisEmpty> p=new ArrayList<>();
+
+        //check ArrayList is empty
+        System.out.println(p.isEmpty());
+        p.add(new PersonisEmpty("java",3));
+        System.out.println(p.isEmpty());
+    }
+}
